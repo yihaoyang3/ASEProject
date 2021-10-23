51e3c046a9a462aeaf5303d466fb227ea659dcfc
Jacqueline <1007579629@qq.com>
2021-10-23 19:50:57 -0400

    JavaBean

--- src/main/java/com/aseproject/domain/MapStorageInfo.java
+++ src/main/java/com/aseproject/domain/MapStorageInfo.java
@@ -1,5 +1,33 @@
 package com.aseproject.domain;
 
-public class MapStorageInfo
+public class MapStorageInfo //JavaBean
 {
+    private String mapId;
+    private String mapStorageName;
+    private String mapStoragePath;
+
+    public MapStorageInfo(){
+        super();
+    }
+
+    public String getMapId(){
+        return mapId;
+    }
+    public void setMapId(String mapId){
+        this.mapId = mapId;
+    }
+
+    public String getMapStorageName(){
+        return mapStorageName;
+    }
+    public void setMapStorageName(String mapStorageName){
+        this.mapStorageName = mapStorageName;
+    }
+
+    public String getMapStoragePath(){
+        return mapStoragePath;
+    }
+    public void setMapStoragePath(String mapStoragePath){
+        this.mapStoragePath = mapStoragePath;
+    }
 }
