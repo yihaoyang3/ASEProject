f9029af8090cf917a0781be291adf530faf6a22a
GravityDust <yyh64549@outlook.com>
2021-10-24 22:43:05 +0800

    modify

--- src/main/java/com/aseproject/service/MapStorageInfoService.java
+++ src/main/java/com/aseproject/service/MapStorageInfoService.java
@@ -1,5 +1,45 @@
 package com.aseproject.service;
 
-public class MapStorageInfoService
-{
+
+import com.google.gson.Gson;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+class mapwithcoordinates{
+    public int coordinates[];
+    public String data;
+}
+
+public class MapStorageInfoService {
+    public void storeMapInLocal(String[][] mapBlock) throws IOException {
+        Gson gson = new Gson();
+        int row = mapBlock.length;
+        int col = mapBlock[0].length;
+        int len = row * col;
+        mapwithcoordinates[] newc = new mapwithcoordinates[len];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                mapwithcoordinates c = new mapwithcoordinates();
+                c.coordinates[0] = i;
+                c.coordinates[1] = j;
+                c.data = mapBlock[i][j];
+                newc[i] = c;
+            }
+        }
+        String json = gson.toJson(newc);
+
+        File file = new File("mapwithcoordinates");
+        if(!file.exists()){
+            file.createNewFile();
+        }
+        String j =json.toString();
+        byte[] b = j.getBytes();
+        int l = j.length();
+        OutputStream os = new FileOutputStream(file);
+        os.write(b,0,l);
+        os.close();
+    }
 }
