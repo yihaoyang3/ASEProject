a97913aaa2e23a892947edb4485f26e5062a614f
GravityDust <yyh64549@outlook.com>
2021-10-25 19:50:24 +0800

    Revert "Update MapStorageInfoDao.java"
    This reverts commit f4172bb1f98b49ea1dc19cac29af87396f7ed54b.

--- src/main/java/com/aseproject/dao/MapStorageInfoDao.java
+++ src/main/java/com/aseproject/dao/MapStorageInfoDao.java
@@ -1,36 +1,5 @@
 package com.aseproject.dao;
 
-import com.aseproject.domain.MapStorageInfo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-
-public class MapStorageInfoDao 
+public class MapStorageInfoDao
 {
-	@Autowired
-	JdbcTemplate jdbcTemplate;
-	
-	public MapStorageInfoDao()
-	{
-		
-	}
-
-	public MapStorageInfo readMap(String id) {
-		String query = "SELECT * FROM map_storage_info map_id = ?";		
-		var info = (MapStorageInfo) jdbcTemplate.queryForObject(query, new Object[]{id},
-                BeanPropertyRowMapper.newInstance(MapStorageInfo.class));
-		return info;
-	}
-
-	public void deleteMap(String mapId) {
-		String query = "DELETE FROM map_storage_info WHERE map_id = ?";
-		jdbcTemplate.update(query,mapId);	
-	}
-
-	public void addMap(MapStorageInfo info) {
-		String query = "insert into map_storage_info values (?, ?, ?)";
-		jdbcTemplate.update(query,info.getmapId(),info.getmapstoragePath(),info.getmapstorageName());	
-	}
-
 }
