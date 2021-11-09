ce66a015461edb701945f35f4e0af7dcb7a6d06f
GravityDust <yyh64549@outlook.com>
2021-11-09 23:02:53 +0800

    Sing in and up function

---
+++ src/main/java/com/aseproject/util/IdBuilder.java
@@ -0,0 +1,12 @@
+package com.aseproject.util;
+
+import java.util.UUID;
+
+public class IdBuilder
+{
+    public static String generateId()
+    {
+        String s = UUID.randomUUID().toString();
+        return s.substring(0,8)+s.substring(9,13)+s.substring(14,18)+s.substring(19,23)+s.substring(24);
+    }
+}
