21b5d52ae7a776a835c15ed1246b5faf7c8021ac
30Judy <shen13915096972@gmail.com>
2021-11-21 20:27:54 -0500

    Merge remote-tracking branch 'origin/dev' into dev

---
+++ src/main/java/com/aseproject/controller/MapQueryController.java
@@ -0,0 +1,4 @@
+package com.aseproject.controller;
+
+public class MapQueryController {
+}
--- src/main/java/com/aseproject/service/LocationInfoService.java
+++ src/main/java/com/aseproject/service/LocationInfoService.java
@@ -1,5 +1,8 @@
 package com.aseproject.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public class LocationInfoService
 {
+
 }
