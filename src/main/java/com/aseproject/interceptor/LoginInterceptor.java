8fb6ad544361d55fd67f87899b31dbc5634e4dcf
Yihao Yang <yyh64549@outlook.com>
2021-11-25 19:46:46 +0800

    authority management repair

--- src/main/java/com/aseproject/interceptor/LoginInterceptor.java
+++ src/main/java/com/aseproject/interceptor/LoginInterceptor.java
@@ -6,13 +6,15 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class LoginInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object Handler) throws Exception {
-        Object hadLogin = request.getSession().getAttribute("isLoggedIn");
-        if (hadLogin == null || hadLogin.equals("")) {
+        HttpSession session = request.getSession(false);
+        boolean isLoggedIn = (boolean) session.getAttribute("isLoggedIn");
+        if (!isLoggedIn) {
             request.setAttribute("msg", "You must login before upload maps!");
             request.getRequestDispatcher("/login").forward(request, response);
             return false;
@@ -20,10 +22,4 @@ public class LoginInterceptor implements HandlerInterceptor {
             return true;
         }
     }
-
-    @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {}
-
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {}
 }
