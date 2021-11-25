b03554694c472c199b233b260d66278ca38978e8
30Judy <shen13915096972@gmail.com>
2021-11-25 02:15:29 -0500

    Merge remote-tracking branch 'origin/dev' into dev

--- src/main/java/com/aseproject/dao/MapDao.java
+++ src/main/java/com/aseproject/dao/MapDao.java
@@ -12,41 +12,41 @@ public class MapDao
 {
 
     @Autowired
-    JdbcTemplate jdbcTemplate;
+    JdbcTemplate  jdbcTemplate;
 
     // insert, add mapStorageInfo
     public void addMap(MapStorageInfo info)
     {
         String sql = "insert into map_storage_info(map_id, map_storage_name, map_storage_path) values (?,?,?)";
-        jdbcTemplate.update(sql, info.getMapId(), info.getMapStorageName(), info.getMapStoragePath());
+        jdbcTemplate.update( sql, info.getMapId(), info.getMapStorageName(), info.getMapStoragePath());
     }
 
-    // delete
-    public void deleteMap(String mapId)
+    // delete map information in sql
+    public void  deleteMap(String mapId)
     {
-        String sql = "delete from map_storage_info where map_id = ?";
-        jdbcTemplate.update(sql, mapId);
+        String sql =  "delete from map_storage_info where map_id = ?";
+        jdbcTemplate.update( sql, mapId);
     }
 
-    public List<HashMap<String, String>> queryMapByName(String query)
+    public List< HashMap<String, String>> queryMapByName(String query)
     {
-        List<HashMap<String, String>> mapSet = new LinkedList<>();
+        List< HashMap<String, String>> mapSet = new LinkedList<>();
 
         // default: select all (query is empty)
-        String sql = "SELECT map_id, map_name FROM map_storage_info";
+        String sql = " SELECT map_id, map_name FROM map_storage_info";
 
-        // if query is not empty
+        // when query is not empty
         if (query != null || query != "") {
             String regex = "\\w*" + query + "\\w*";
             sql = "SELECT map_id, map_name FROM map_storage_info WHERE map_name REGEXP " + regex;
         }
 
-        // put all result into list
-        List<Map<String, Object>> rows = jdbcTemplate.queryForList(sql);
+        // put all result into a list
+        List<Map <String, Object>> rows = jdbcTemplate.queryForList(sql);
         for (Map row : rows) {
-            HashMap<String, String> item = new HashMap<>() {{
-                put("mapID", (String) row.get("map_id"));
-                put("mapName", (String) row.get("map_name"));
+            HashMap <String, String> item = new HashMap<>() {{
+                put( "mapID", (String) row.get("map_id"));
+                put( "mapName", (String) row.get("map_name"));
             }};
             mapSet.add(item);
         }
@@ -55,22 +55,23 @@ public class MapDao
 
     public List<Map<String, String>> getAllMapsById()
     {
-        List<Map<String, String>> mapSet = new LinkedList<>();
-        String sql = "select map_id, map_storage_name from map_storage_info";
-        jdbcTemplate.query(sql, resultSet -> {
+        List<Map <String, String>> mapSet = new LinkedList<>();
+        String sql = " select map_id, map_storage_name from map_storage_info";
+        jdbcTemplate.query( sql, resultSet -> {
 
-            HashMap<String, String> mapObject = new HashMap<>();
-            mapObject.put("id", (String) resultSet.getObject("map_id"));
-            mapObject.put("name", (String) resultSet.getObject("map_storage_name"));
-            mapSet.add(mapObject);
+            HashMap <String, String> mapObject = new HashMap<>();
+            mapObject.put( "id", (String) resultSet.getObject("map_id"));
+            mapObject.put( "name", (String) resultSet.getObject("map_storage_name"));
+            mapSet.add( mapObject);
         });
         return mapSet;
     }
 
+    //get storage path
     public String queryMapPathById(String id)
     {
         String sql = "select map_storage_path from map_storage_info where map_id = ?";
-        String storagePath = jdbcTemplate.queryForObject(sql, String.class,id);
+        String storagePath = jdbcTemplate.queryForObject( sql, String.class,id);
         return storagePath;
     }
 }
