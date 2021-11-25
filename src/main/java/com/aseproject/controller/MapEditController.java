e1bfe54a8b0852bdbecee4d96c0648bc5e8f98eb
Yihao Yang <yyh64549@outlook.com>
2021-11-25 18:51:22 +0800

    Test for upload locationItem

--- src/main/java/com/aseproject/controller/MapEditController.java
+++ src/main/java/com/aseproject/controller/MapEditController.java
@@ -15,13 +15,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Map;
 
 @Controller
 public class MapEditController
@@ -40,7 +40,8 @@ public class MapEditController
     {
         JSONArray locSet = new JSONArray();
         ArrayList<LocationInfo> locList = locationInfoDao.selectLocByMapId(mapId);
-        for (LocationInfo loc : locList) {
+        for (LocationInfo loc : locList)
+        {
             JSONObject obj = new JSONObject();
             obj.put("loc_id", loc.getLocationId());
             obj.put("loc_name", loc.getLocationName());
@@ -55,8 +56,8 @@ public class MapEditController
 
     @PostMapping("/saveVideo")
     @ResponseBody
-    public String saveVideoHandler(@RequestParam("file") MultipartFile file)
-            throws IllegalStateException, IOException {
+    public String saveVideoHandler(@RequestParam("file") MultipartFile file) throws IllegalStateException, IOException
+    {
         String msg = locationInfoService.saveLocVideo(file);
         Gson gson = new Gson();
         return gson.toJson(msg);
@@ -64,18 +65,41 @@ public class MapEditController
 
     @RequestMapping(value = "/showVideo", method = RequestMethod.GET)
     @ResponseBody
-    public void getVideoHandler(@RequestParam("loc_file_path") String path, HttpServletResponse response) {
-        try {
+    public void getVideoHandler(@RequestParam("loc_file_path") String path, HttpServletResponse response)
+    {
+        try
+        {
             File file = new File(path);
             response.setContentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
-            response.setHeader("Content-Disposition", "attachment; filename="+file.getName().replace(" ", "_"));
+            response.setHeader("Content-Disposition", "attachment; filename=" + file.getName().replace(" ", "_"));
             InputStream iStream = new FileInputStream(file);
             IOUtils.copy(iStream, response.getOutputStream());
             response.flushBuffer();
-        } catch (java.nio.file.NoSuchFileException e) {
+        } catch (java.nio.file.NoSuchFileException e)
+        {
             response.setStatus(HttpStatus.NOT_FOUND.value());
-        } catch (Exception e) {
+        } catch (Exception e)
+        {
             response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        }
+    }
+
+    /**
+     *
+     * @param itemInfo should include two files: mapId and locationName
+     * @param media include one video or several images
+     * @return
+     */
+    @RequestMapping("/mapEdit/addLocationItem")
+    @ResponseBody
+    public String addLocationItem(@RequestParam("itemInfo") Map<String, Object> itemInfo, @RequestParam("media") MultipartFile[] media)
+    {
+        if (itemInfo != null&& media != null)
+        {
+            return "Success";
+        }else
+        {
+            return "No data";
         }
     }
 
