affa916a2721317e6623b378829ae93a9d460afd
Yicheng Lu <Dereklu1996@gmail.com>
2021-12-04 12:58:22 -0500

    Adding libraries

---
+++ src/main/java/com/aseproject/domain/MapBlockInfo.java
@@ -0,0 +1,26 @@
+package com.aseproject.domain;
+
+public class MapBlockInfo {
+
+    private int[] coordinates;
+    private String data;
+
+    public MapBlockInfo(int[] coord, String dat){
+        this.coordinates = coord;
+        this.data = dat;
+    }
+
+    public int[] getCoordinate() { return this.coordinates; }
+
+    public void setCoordinate(int[] coord) {
+        this.coordinates = coord;
+    }
+
+    public String getData() {
+        return this.data;
+    }
+
+    public void setData(String dat) {
+        this.data = dat;
+    }
+}
